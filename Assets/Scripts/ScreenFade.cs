d7dd4ca97b00a33a899f7db60b8e8ba8859564f9
Umutkok <umutkok88@gmail.com>
2024-01-04 01:12:58 +0300

    lavel design - umut 2

--- Assets/Scripts/ScreenFade.cs
+++ Assets/Scripts/ScreenFade.cs
@@ -3,19 +3,19 @@ using UnityEngine.UI;
 
 public class ScreenFade : MonoBehaviour
 {
-    [SerializeField] Image FadeImage;
+    //[SerializeField] Image FadeImage;
 
-    private void Update()
-    {
-        //fade out
-        if (FadeImage.color.a >= 0)
-        {
-            FadeImage.color = new Color(0, 0, 0, FadeImage.color.a + Time.deltaTime/5);
-        }
-    }
+    //private void Update()
+    //{
+    //    //fade out
+    //    if (FadeImage.color.a >= 0)
+    //    {
+    //        FadeImage.color = new Color(0, 0, 0, FadeImage.color.a + Time.deltaTime/5);
+    //    }
+    //}
 
-    public void FadeIn()
-    {
-        FadeImage.color = new Color(0, 0, 0, 1);
-    }
+    //public void FadeIn()
+    //{
+    //    FadeImage.color = new Color(0, 0, 0, 1);
+    //}
 }
