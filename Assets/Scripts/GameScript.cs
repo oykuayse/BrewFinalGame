b896552a60866cb841d54f0a2ae8629a2eedc3f9
Umutkok <umutkok88@gmail.com>
2024-01-03 22:50:19 +0300

    fade

--- Assets/Scripts/GameScript.cs
+++ Assets/Scripts/GameScript.cs
@@ -29,7 +29,5 @@ public class GameScript : MonoBehaviour
             yield return null;
         }
 
-
-        SceneManager.LoadScene("0");
     }
 }
--- Assets/Scripts/ScreenFade.cs
+++ Assets/Scripts/ScreenFade.cs
@@ -1,71 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScreenFade : MonoBehaviour
 {
-    public float fadeDuration = 60.0f;
-    public float fadeResetDelay = 10.0f;
-    public Material fadeMaterial;
+    [SerializeField] Image FadeImage;
 
-    private bool isFading = false;
-    private bool isResetting = false;
-    private float fadeTimer = 0.0f;
-    private float resetTimer = 0.0f;
-
-    void Start()
+    private void Update()
     {
-        StartCoroutine(FadeOut());
-    }
-
-    void Update()
-    {
-        if (isResetting)
+        //fade out
+        if (FadeImage.color.a >= 0)
         {
-            resetTimer += Time.deltaTime;
-            if (resetTimer >= fadeResetDelay)
-            {
-                resetTimer = 0.0f;
-                isResetting = false;
-                StartCoroutine(FadeOut());
-            }
+            FadeImage.color = new Color(0, 0, 0, FadeImage.color.a + Time.deltaTime/5);
         }
     }
 
-    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    public void FadeIn()
     {
-        if (isFading)
-        {
-            fadeMaterial.SetFloat("_FadeAmount", Mathf.Clamp01(fadeTimer / fadeDuration));
-            Graphics.Blit(src, dest, fadeMaterial);
-        }
-        else
-        {
-            Graphics.Blit(src, dest);
-        }
-    }
-
-    IEnumerator FadeOut()
-    {
-        isFading = true;
-        while (fadeTimer < fadeDuration)
-        {
-            fadeTimer += Time.deltaTime;
-            yield return null;
-        }
-        isFading = false;
-        isResetting = true;
-    }
-
-    public void CollectMushroom()
-    {
-
-        StopAllCoroutines();
-        isFading = false;
-        isResetting = false;
-        fadeTimer = 0.0f;
-        resetTimer = 0.0f;
-        StartCoroutine(FadeOut());
+        FadeImage.color = new Color(0, 0, 0, 1);
     }
 }
